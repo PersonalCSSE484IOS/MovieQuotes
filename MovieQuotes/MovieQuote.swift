5d119637df5f1eb165fa7ffb144922061d1d6359
rhit-rykerzhang <52553132+rhit-rykerzhang@users.noreply.github.com>
2022-03-28 09:36:20 -0400

    progress at alert controller

---
+++ MovieQuotes/MovieQuote.swift
@@ -0,0 +1,20 @@
+//
+//  MovieQuote.swift
+//  MovieQuotes
+//
+//  Created by Yujie Zhang on 3/28/22.
+//
+
+
+
+import Foundation
+
+class MovieQuote{
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String){
+        self.quote = quote
+        self.movie = movie
+    }
+}
